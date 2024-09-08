70dd527d3b33abcb8d35523548ac3b755a1c8d22
negr <martinpolakmp@seznam.cz>
2024-09-08 17:39:20 +0200

    j

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,16 @@
+﻿string uzivateldobre = "negr";
+string heslodobre = "negrnegr";
+
+Console.WriteLine("zadej uziv jsmeno");
+string uzivatel = (Console.ReadLine());
+Console.WriteLine("zadej heslo");
+string heslo = (Console.ReadLine());
+
+if (uzivatel == uzivateldobre && heslo == heslodobre)
+{
+    Console.WriteLine("vítejte");
+}
+else
+{
+    Console.WriteLine("ses pica a mas tam spatny heslo");
+}
